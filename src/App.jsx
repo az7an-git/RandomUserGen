68a683258cdcdb9b142e4b13c9a6229972d800d9
Azlan Jalil <azlanjalil543@gmail.com>
2025-11-19 13:30:39 +0500

    New user Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { RandomUser } from "./api/RandomUser";
+import "./App.css";
+import { useEffect, useState } from "react";
+import UserCard from "./Components/UserCard";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    RandomUser().then((user) => setUserData(user.results[0]));
+  }, []);
+
+  const refresh = () => {
+    RandomUser().then((user) => setUserData(user.results[0]));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-col justify-center items-center h-dvh ">
+      {userData && <UserCard data={userData} />}
+      <button
+        onClick={refresh}
+        className="p-2 mt-4 bg-amber-400 px-4 rounded-lg hover:bg-amber-300"
+      >
+        New User
+      </button>
+    </div>
+  );
 }
-
-export default App
+export default App;
