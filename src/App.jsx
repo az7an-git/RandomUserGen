3bb749684b3a487d2b5c59243db36ae5fe14e7c0
Azlan Jalil <azlanjalil543@gmail.com>
2025-11-22 12:55:55 +0500

    Grid for cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,50 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import UserCard from "./Components/UserCard";
+import SkeletonCard from "./Components/SkeletonCard";
 import { RandomUser } from "./api/RandomUser";
 
 function App() {
-  const [userData, setUserData] = useState(null);
+  const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const loadUser = async () => {
+  const loadUsers = async () => {
     try {
       setLoading(true);
       setError(null);
-      const user = await RandomUser();
-      setUserData(user.results[0]);
+
+      const data = await RandomUser(6); // fetch 6 users
+      setUsers(data.results);
     } catch (e) {
-      console.error("Failed to fetch user:", e);
-      setError("Failed to load user. Try again.");
+      setError("Failed to load users.", e);
     } finally {
       setLoading(false);
     }
   };
 
   useEffect(() => {
-    loadUser();
+    loadUsers();
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-linear-to-br from-slate-200 via-slate-300 to-slate-500 p-6">
-      {loading ? (
-        <div className="flex flex-col items-center animate-pulse">
-          <div className="w-20 h-20 rounded-full bg-gray-300 mb-4"></div>
-          <p className="text-xl font-semibold text-gray-700">Loading...</p>
-        </div>
-      ) : (
-        userData && <UserCard data={userData} />
-      )}
+    <div className="min-h-screen bg-linear-to-br from-slate-200 via-slate-300 to-slate-500 p-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 place-items-center">
+        {loading
+          ? Array.from({ length: 6 }).map((_, i) => <SkeletonCard key={i} />)
+          : users.map((user, idx) => <UserCard key={idx} data={user} />)}
+      </div>
 
-      {error && <p className="text-red-500 mt-2 font-medium">{error}</p>}
+      {error && <p className="text-red-500 mt-4 text-center">{error}</p>}
 
-      <button
-        onClick={loadUser}
-        className="mt-5 px-6 py-3 bg-amber-500 hover:bg-amber-400 active:scale-95 transition-all text-white rounded-xl shadow-md font-medium"
-      >
-        New User
-      </button>
+      <div className="flex justify-center mt-6">
+        <button
+          onClick={loadUsers}
+          className="px-6 py-3 bg-amber-500 hover:bg-amber-400 rounded-xl text-white shadow-md active:scale-95 transition"
+        >
+          Load New Users
+        </button>
+      </div>
     </div>
   );
 }
