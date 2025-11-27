ef5a16f5e550badd70a978825a8afa167768cbda
Azlan Jalil <azlanjalil543@gmail.com>
2025-11-27 13:44:49 +0500

    DRY Code

---
+++ src/Components/UserList.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import { UserContext } from "../context/userContext";
+import UserCard from "./UserCard";
+import SkeletonCard from "./SkeletonCard";
+
+export const UserList = () => {
+  const { filteredUsers, loading } = useContext(UserContext);
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 place-items-center">
+      {loading
+        ? Array.from({ length: 6 }).map((_, i) => <SkeletonCard key={i} />)
+        : filteredUsers.map((user) => (
+            <UserCard key={user.login.uuid} data={user} />
+          ))}
+    </div>
+  );
+};
+
+export default UserList;
